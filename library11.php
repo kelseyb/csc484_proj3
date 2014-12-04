c85d78a4fadb671f6d364aa352094129b9f0006a
kelseyb <kelsey.bellew@mines.sdsmt.edu>
2014-12-04 13:31:44 -0700

    broke things?

--- library11.php
+++ library11.php
@@ -1,7 +1,7 @@
 <HTML>
 <HEAD>
 <TITLE>
-Video Status
+Book Status
 </TITLE>
 </HEAD>
 <BODY bgcolor = wheat >
@@ -43,6 +43,7 @@ echo "</TR>";
 
 /* Fetch and displays each row of $result */ 
 if($result)
+{
    while($row=mysql_fetch_row($result))
    {
       echo "<TR>";
@@ -52,9 +53,74 @@ if($result)
       }
       echo "</TR>\n";
    }
+}//added
+
+if (isset($_POST['search']))
+{
+   $id = 0;
+   $title = $_POSN['title']
+   //$fname = $_POST['fname'];
+	//$lname = $_POST['lname'];
+	$query = "SELECT copyID, title FROM CopyBook, Book WHERE title LIKE '%$title%' AND copyID > $id"; //this doesnt make sense
+   //$query = "SELECT patronID, patronName, patronType FROM Patron WHERE patronName LIKE '%$name%' AND patronID > $id";
+   //$query = "SELECT MEMBERID, FNAME, LNAME, ADDRESS, DATEJOINED, PHONENO FROM Member WHERE FNAME LIKE '%$fname%' AND LNAME LIKE '%$lname%' AND MEMBERID > $id";
+   $res = mysql_query($query);
+   $row = mysql_fetch_row($res);
+   if ($row[0] > 0)
+   {
+       $id      = $row[0];
+       $title   = $row[1];
+    }
+}
+
+// elseif (isset($_POST['loan']))
+// {
+//   $id = 0;
+//   //??
+//   $name = $_POSN['name']
+//   //$fname = $_POST['fname'];
+// 	//$lname = $_POST['lname'];
+//   $query = "SELECT patronID, patronName, patronType, FROM Patron WHERE patronName LIKE '%$name%' AND patronID > $id";
+//   //$query = "SELECT MEMBERID, FNAME, LNAME, ADDRESS, DATEJOINED, PHONENO FROM Member WHERE FNAME LIKE '%$fname%' AND LNAME LIKE '%$lname%' AND MEMBERID > $id";
+//   $res = mysql_query($query);
+//   $row = mysql_fetch_row($res);
+//   if ($row[0] > 0)
+//   {
+//       $id      = $row[0];
+//       $name   = $row[1];
+//       $type   = $row[2];
+//     }
+// }
 
 mysql_close($link);
 ?>
+
+
+<BR> Patron ID:
+<BR><INPUT TYPE="TEXT" NAME="id"
+    <?php echo "VALUE=\"$id\"" ?>>
+<BR>
+<BR> Name:
+<BR><INPUT TYPE="TEXT" NAME="title"
+    <?php echo "VALUE=\"$name\"" ?>>
+<BR>
+<BR> Type:
+<BR><INPUT TYPE="TEXT" NAME="authorName"
+    <?php echo "VALUE=\"$type\"" ?>>
+<BR>
+<BR>
+
+<INPUT TYPE="SUBMIT" NAME="search"     VALUE="Search">
+<INPUT TYPE="SUBMIT" NAME="loan"     VALUE="Loan">
+
+<?php
+if (isset($_POST['message']))
+{
+   echo "<BR><BR>$message";
+}
+?>
+
+<BR>
 
 </TABLE>
 <BR>
